ca40cf73416718f773c4ad1c09b8f6987e004d47
Devin <71891263+devincar@users.noreply.github.com>
2022-04-14 16:53:56 -0400

    push before I break it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SideBar from './components/SideBar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
+import Login from './Pages/Login';
 import Education from './Pages/Education';
 import Work from './Pages/Work';
 import Activities from './Pages/Activities';
@@ -15,6 +16,7 @@ function App() {
     <Router>
       <SideBar />
       <Switch>
+        <Route path='/' exact component={Login}/>
         <Route path='/education' exact component={Education}/>
         <Route path='/work' component={Work}/>
         <Route path='/activities' component={Activities}/>
---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {useRef, useState, useEffect} from 'react';
+
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+  return (
+    <section>
+        <p ref={errRef} className={errMsg ? "errMsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Sign In</h1>
+        <form>
+            <label htmlFor="username">Username:</label>
+            <input 
+                type="text" 
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+            />
+        </form>
+    </section>
+  )
+}
+
+export default Login
